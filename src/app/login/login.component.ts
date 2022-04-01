a097a4856be5bc7116cf0996643bec39db6192c2
Luiz Felipe Henrique Pereira <lfhenrique95@gmail.com>
2022-04-01 14:54:41 -0300

    @LuiizHenrique [Task:Criando Rotas no Login]

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { UserLogin } from '../model/UserLogin';
 import { AuthService } from '../service/auth.service';
 
 @Component({
@@ -7,9 +10,11 @@ import { AuthService } from '../service/auth.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  userLogin:UserLogin = new UserLogin()
 
   constructor(
-    private auth: AuthService
+    private auth: AuthService,
+    private router: Router
   ) { }
 
   ngOnInit() {
@@ -17,9 +22,20 @@ export class LoginComponent implements OnInit {
   }
 
   entrar(){
-
-
-
+    this.auth.login(this.userLogin).subscribe({
+      next:(resp: UserLogin)=>{
+        this.userLogin = resp
+        environment.token = this.userLogin.token
+        environment.nome = this.userLogin.nome
+        environment.foto = this.userLogin.foto
+        environment.id = this.userLogin.id
+        this.router.navigate(['/inicio'])
+      },
+      error: erro =>{
+        if (erro.status == 500){
+          alert('Usuário ou senha inválidos')
+        }
+      },
+    });
   }
-
 }
