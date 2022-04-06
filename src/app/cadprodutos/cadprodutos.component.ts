8c90e2723dc1f18a8fb8318be9a7ee4bd6440dae
Ingrid Kis <ingrid.kr@hotmail.com>
2022-04-06 15:44:03 -0300

    @ingrid-kis [TASK: Cadprodutos n Login html]

--- src/app/cadprodutos/cadprodutos.component.ts
+++ src/app/cadprodutos/cadprodutos.component.ts
@@ -24,10 +24,10 @@ export class CadprodutosComponent implements OnInit {
   ngOnInit(){
     window.scroll(0,0)
 
-    // if(environment.token == ""){
-    //   alert("Sua seção expirou, faça o login novamente.")
-    //   this.router.navigate(["/login"])
-    // }
+    if(environment.token == ""){
+      alert("Sua seção expirou, faça o login novamente.")
+      this.router.navigate(["/login"])
+    }
 
     this.produtoService.refreshToken()
   }
